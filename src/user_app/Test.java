00174fae61b9773b4ea93a021f555b1f8cc16b8e
Tokimahery Ramarozaka <tokyramarozaka@gmail.com>
2023-07-04 17:02:05 +0300

    feat: findByUserId

--- src/user_app/Test.java
+++ src/user_app/Test.java
@@ -3,4 +3,7 @@ package user_app;
 import java.sql.Connection;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(UserCRUD.findUserById(1));
+    }
 }
--- src/user_app/UserCRUD.java
+++ src/user_app/UserCRUD.java
@@ -1,6 +1,7 @@
 package user_app;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -8,10 +9,16 @@ import java.util.List;
 public class UserCRUD {
     // insert a user
     public static void insertUser(int id, String name, String email, String password){
-
+        /**
+         * Devoir : faites en sorte que le CRUD marche
+         * - Trouvez un moyen pour qu'on n'utilise plus qu'une SEULE instance de
+         * Connection, et une SEULE instance de Statement dans TOUTE l'application.
+         * Pour cela, regardez le DESIGN PATTERN : SINGLETON.
+         */
     }
 
     // find a single user by its id
+    // en JPA vous auriez juste écrit : public User findById(int id);
     public static User findUserById(int id){
         ConnectionToDatabase db = new ConnectionToDatabase();
         Connection connection = db.createConnection();
@@ -19,11 +26,20 @@ public class UserCRUD {
         try{
             Statement statement = connection.createStatement();
             String sql = "SELECT * FROM \"user\" WHERE id = "+id;
+            ResultSet resultSet = statement.executeQuery(sql);
 
+            if(resultSet.next()){
+                return new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")
+                );
+            }
         }catch (SQLException e){
             e.printStackTrace();
         }
-        return null;
+        return null; // il n'y a pas d'utilisateur avec cet id
     }
 
     // find all users
