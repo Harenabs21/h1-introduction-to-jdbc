b4c2dbc2e0e03930f8bb22cebcca85fcf8e26be0
Harenabs21 <hei.harena.6@gmail.com>
2023-07-07 12:05:21 +0300

    fix:java.sql import statement

--- src/user_app/UserCRUD.java
+++ src/user_app/UserCRUD.java
@@ -1,9 +1,6 @@
 package user_app;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class UserCRUD {
