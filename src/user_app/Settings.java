65451003e3141d8414acb8c552a4d16741da0bd9
Harenabs21 <hei.harena.6@gmail.com>
2023-07-07 11:47:37 +0300

    fix:password stings file

--- src/user_app/Settings.java
+++ src/user_app/Settings.java
@@ -3,5 +3,5 @@ package user_app;
 public class Settings {
     public static final String DATABASE_NAME = "user_app";
     public static final String USER = "postgres";
-    public static final String PASSWORD = "archfiend";
+    public static final String PASSWORD = "psql";
 }
